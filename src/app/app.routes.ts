bd789522f3e585806898caaab7d3869b71bbd1b9
Swatheshson <swathesh.ags@gmail.com>
2025-07-23 15:54:53 +0530

    shifted page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home} from '../home/home';
+import { DcPage } from '../dc-page/dc-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'home',component:Home},
+    {path: 'dcpage',component:DcPage},
+
+    {path:'',redirectTo:'home',pathMatch:'full'}
+];
+
+
