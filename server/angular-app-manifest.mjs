14fdd344872905a48b21d6506983210c899e1f05
Swatheshson <swathesh.ags@gmail.com>
2025-08-08 16:50:37 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -16,7 +16,7 @@ export default {
   },
   {
     "renderMode": 2,
-    "route": "/movie_site/dc"
+    "route": "/movie_site/dcpage"
   },
   {
     "renderMode": 2,
@@ -26,10 +26,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5038, hash: '55a0d921d4c9c294147862f44299f6b1558975d0613c137734e2fc88b3881e23', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1010, hash: '840de867e2dc8e1f1852d05190282d93658c40ae0ebc182a39da70f085b3c987', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'dc/index.html': {size: 21577, hash: '576eb06656153999c1b881397522e51b3a67da1e3b9b6dee4cc66944432cc547', text: () => import('./assets-chunks/dc_index_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 19488, hash: '55467de54e9b990f640234eaa3c2459c11f177714c76c92c94a369872d4c1c11', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5038, hash: '96a1952e741fd947acd81cd1641a9684717f814f2ce3697dfbeded1261d48e60', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1010, hash: '52a14735327cce917e37ad5454faaea7d3f7d9485993a65cadda808b0e811dfb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 19488, hash: '897b869f259635bf237dc7722ceb086ae8850eb3a60bfe028f74bf0a63148a04', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'dcpage/index.html': {size: 21577, hash: '3d0e0c953cd646fb0b9c6ddd56eac102d5226852e89e18ded9c080f6cce40ba6', text: () => import('./assets-chunks/dcpage_index_html.mjs').then(m => m.default)},
     'styles-BVJQD57C.css': {size: 230873, hash: 'YU+im7r2LDs', text: () => import('./assets-chunks/styles-BVJQD57C_css.mjs').then(m => m.default)}
   },
 };
